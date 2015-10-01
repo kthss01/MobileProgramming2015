2b9ba454ee39b26fff8da0907f4d1170f82cf2e3
Kay <kthss01@naver.com>
2015-10-01 13:49:05 +0900

    lab04 push 시도2

---
+++ src/kr/ac/embedded/kookmin/sales/DiscountSale.java
@@ -0,0 +1,60 @@
+package kr.ac.embedded.kookmin.sales;
+
+/**
+ * Class for a sale of one item with discount expressed as a percent of the
+ * price, but no other adjustments. Class invariant: The price is always
+ * nonnegative; the name is a nonempty string; the discount is always
+ * nonnegative.
+ */
+
+public class DiscountSale extends Sale {
+	private double	discount;	// A percent of the price. Cannot be negative.
+								
+	public DiscountSale() {
+		/** 구현 하시오 **/ 
+	}
+	
+	/**
+	 * Precondition: theName is a nonempty string; thePrice is nonnegative;
+	 * theDiscount is expressed as a percent of the price and is nonnegative.
+	 */
+	public DiscountSale(String theName, double thePrice, double theDiscount) {
+		/** 구현 하시오 **/ 
+	}
+	
+	public DiscountSale(DiscountSale originalObject) {
+		/** 구현 하시오 **/ 
+	}
+	
+	public static void announcement() {
+		System.out.println("This is the DiscountSale class.");
+	}
+	
+	public double bill() {
+		/** 구현 하시오 **/ 
+	}
+	
+	public double getDiscount() {
+		/** 구현 하시오 **/ 
+	}
+	
+	/**
+	 * Precondition: Discount is nonnegative.
+	 */
+	public void setDiscount(double newDiscount) {
+		/** 구현 하시오 **/ 
+	}
+	
+	public String toString() {
+		return (getName() + " Price = $" + getPrice() + " Discount = " + discount + "%\n" + "   Total cost = $" + bill());
+	}
+	
+	public boolean equals(Object otherObject) {
+		/** 구현 하시오 **/ 
+	}
+	
+	
+	public DiscountSale clone() {
+		/** 구현 하시오....  임시생성자 사용 **/ 
+	}
+}
